17b7e26653a77e048f4a0021d392d646e3d1903c
Val <valentin.delaunay1@gmail.com>
2020-05-16 00:02:12 +0200

    Affichage des données d'un compte

--- src/main/java/fr/vdelll/onlinebank/web/BanqueController.java
+++ src/main/java/fr/vdelll/onlinebank/web/BanqueController.java
@@ -1,9 +1,13 @@
 package fr.vdelll.onlinebank.web;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import fr.vdelll.onlinebank.entities.Compte;
+import fr.vdelll.onlinebank.entities.Operation;
 import fr.vdelll.onlinebank.metier.IBanqueMetier;
 
 @Controller
@@ -11,9 +15,37 @@ public class BanqueController {
 
 	@Autowired
 	private IBanqueMetier banqueMetier;
-	
-	@RequestMapping("/operations") 
+
+	@RequestMapping("/operations")
 	public String index() {
+		return "comptes";
+	}
+
+	/**
+	 * Récupération du compte s'il existe. Affichage des informations du compte.
+	 * Affichage de la liste des opérations du compte.
+	 * 
+	 * @param model
+	 * @param codeCpte
+	 * @return
+	 */
+	@RequestMapping("/consultercompte")
+	public String consulter(Model model, String codeCompte) {
+
+		model.addAttribute("codeCompte", codeCompte);
+
+		try {
+			Compte cp = banqueMetier.consulterCompte(codeCompte);
+			
+			// Liste des opérations 
+			Page<Operation> pageOperations = banqueMetier.listOperation(codeCompte, 0, 4);
+			model.addAttribute("listOperations", pageOperations.getContent());
+			
+			model.addAttribute("compte", cp);
+		} catch (Exception e) {
+			model.addAttribute("exception", e);
+		}
+
 		return "comptes";
 	}
 }
