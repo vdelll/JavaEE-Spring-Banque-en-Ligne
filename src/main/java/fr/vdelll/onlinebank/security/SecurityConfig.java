5096798f34bdc1f56964be99a1e86e429cb0e26c
Val <valentin.delaunay1@gmail.com>
2020-05-16 14:50:08 +0200

    Gestion de l'authentification avec attribution de rôles

--- src/main/java/fr/vdelll/onlinebank/security/SecurityConfig.java
+++ src/main/java/fr/vdelll/onlinebank/security/SecurityConfig.java
@@ -1,21 +1,42 @@
 package fr.vdelll.onlinebank.security;
 
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-	
+
+	@Autowired
+	private DataSource dataSource;
+
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		auth.inMemoryAuthentication().withUser("admin").password("{noop}admin").roles("ADMIN", "USER");
-		auth.inMemoryAuthentication().withUser("user").password("{noop}user").roles("USER");
+//		auth.inMemoryAuthentication().withUser("admin").password("{noop}admin").roles("ADMIN", "USER");
+//		auth.inMemoryAuthentication().withUser("user").password("{noop}user").roles("USER");
+
+		auth.jdbcAuthentication().dataSource(dataSource)
+				.usersByUsernameQuery(
+						"SELECT username AS principal, password AS credentials, active FROM users WHERE username=?")
+				.authoritiesByUsernameQuery(
+						"SELECT username AS principal, roles AS role FROM users_roles WHERE username=?")
+				.rolePrefix("ROLE_").passwordEncoder(passwordEncoder());
 	}
-	
+
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.formLogin().loginPage("/login");
